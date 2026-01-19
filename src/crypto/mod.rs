c8006835ca7126eef4ef124557bff43dbc30b3af
Wolfyxon <wolfyxon@gmail.com>
2026-01-19 18:29:42 +0100

    Rng logic

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,3 +1,6 @@
+use aes_gcm::aead::rand_core::{self};
+use rand::{SeedableRng, rngs::StdRng};
+use sha2::{Digest, Sha256};
 use sysinfo::{CpuRefreshKind, MemoryRefreshKind, Networks};
 
 pub mod aes256;
@@ -43,11 +46,47 @@ pub fn get_machine_summary() -> String {
     format!("{arch}|{sys_name}|{host_name}|{distro}|{total_mem}|{component_count}|{total_critical_temp}|{mac_sum}")
 }
 
+pub fn rng_from_string(seed: impl Into<String>) -> StdRng {    
+    let hash = Sha256::digest(seed.into());
+    let seed_buf: [u8; 32] = hash.into();
+
+    StdRng::from_seed(seed_buf)
+}
+
+pub fn get_machine_id_rng() -> StdRng {
+    rng_from_string(get_machine_summary())
+}
+
 #[cfg(test)]
 mod tests {
     use std::{thread, time::Duration};
 
-    use crate::crypto::get_machine_summary;
+    use rand::RngCore;
+
+    use crate::crypto::{get_machine_summary, rng_from_string};
+
+    #[test]
+    fn test_rng() {
+        let seed = "Hello there this is a test seed";
+        
+        let mut rng1 = rng_from_string(seed);
+        let mut rng2 = rng_from_string(seed);
+
+        const LEN: usize = 10;
+
+        let mut buf1: [u8; LEN] = [0; LEN];
+        let mut buf2: [u8; LEN] = [0; LEN];
+        
+        rng1.fill_bytes(&mut buf1);
+        rng2.fill_bytes(&mut buf2);
+        
+        println!("A: {:?}", buf1);
+        println!("B: {:?}", buf2);
+
+        for i in 0..LEN {
+            assert_eq!(buf1[i], buf2[i]);
+        }
+    }
 
     #[test]
     fn test_machine_summary() {
