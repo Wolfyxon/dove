d99434631220641f9319fcec4452755d4721ef93
Wolfyxon <wolfyxon@gmail.com>
2026-01-20 16:41:18 +0100

    Added combine_rich_text()

--- src/utils/ui.rs
+++ src/utils/ui.rs
@@ -1,5 +1,22 @@
-use egui::{Response, Ui};
+use egui::{Align, FontSelection, Response, RichText, Style, Ui, text::LayoutJob};
 
 pub fn input_submitted(resp: &Response, ui: &Ui) -> bool {
     resp.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))
+}
+
+pub fn combine_rich_text(texts: Vec<impl Into<RichText>>) -> LayoutJob {
+    let style = Style::default();
+    let mut layout_job = LayoutJob::default();
+
+    for text in texts {
+        text.into()
+            .append_to(
+                &mut layout_job, 
+                &style, 
+                FontSelection::Default, 
+                Align::Min
+            );
+    }
+
+    layout_job
 }
